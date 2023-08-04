b51881701318c53909d860eeeb7e54584f8715bb
keyahnajanae <keyahna_blanford@hotmail.com>
2023-08-04 18:19:13 -0500

    update

--- models/Property.js
+++ models/Property.js
@@ -1,11 +1,13 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+require('mongoose-currency').loadType(mongoose);
+const Currency = mongoose.Types.Currency;
 
 
 const PropertySchema = new Schema ({
     title: { type: String, required: [true, "Please provide a descriptive title for property"] },
     address: { type: String, required: true},
-    price: Number,
+    price: {type: Currency},
     beds: {type: Number, required: true},
     baths: {type: Number, required: true},
     description: String,
